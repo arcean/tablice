9828a32270b8ac0883d20ae62d83eb386d7eb953
Tomasz Pieniążek <t.pieniazek@gazeta.pl>
2013-04-26 21:50:44 +0200

    Formatting fixes.

--- listmodel.cpp
+++ listmodel.cpp
@@ -10,69 +10,71 @@ ListModel::ListModel(PlateItem* prototype, QObject *parent) :
 
 int ListModel::rowCount(const QModelIndex &parent) const
 {
-  Q_UNUSED(parent);
-  return m_list.size();
+    Q_UNUSED(parent);
+    return m_list.size();
 }
 
 QVariant ListModel::data(const QModelIndex &index, int role) const
 {
-  if(index.row() < 0 || index.row() >= m_list.size())
-    return QVariant();
-  return m_list.at(index.row())->data(role);
+    if (index.row() < 0 || index.row() >= m_list.size())
+        return QVariant();
+
+    return m_list.at(index.row())->data(role);
 }
 
 ListModel::~ListModel() {
-  delete m_prototype;
-  clear();
+    delete m_prototype;
+    clear();
 }
 
 void ListModel::appendRow(PlateItem *item)
 {
-  appendRows(QList<PlateItem*>() << item);
+    appendRows(QList<PlateItem*>() << item);
 }
 
 void ListModel::appendRows(const QList<PlateItem *> &items)
 {
-  beginInsertRows(QModelIndex(), rowCount(), rowCount()+items.size()-1);
-  foreach(PlateItem *item, items) {
-    connect(item, SIGNAL(dataChanged()), SLOT(handleItemChange()));
-    m_list.append(item);
-  }
-  endInsertRows();
+    beginInsertRows(QModelIndex(), rowCount(), rowCount()+items.size()-1);
+
+    foreach (PlateItem *item, items) {
+        connect(item, SIGNAL(dataChanged()), SLOT(handleItemChange()));
+        m_list.append(item);
+    }
+
+    endInsertRows();
 }
 
 void ListModel::insertRow(int row, PlateItem *item)
 {
-  beginInsertRows(QModelIndex(), row, row);
-  connect(item, SIGNAL(dataChanged()), SLOT(handleItemChange()));
-  m_list.insert(row, item);
-  endInsertRows();
+    beginInsertRows(QModelIndex(), row, row);
+    connect(item, SIGNAL(dataChanged()), SLOT(handleItemChange()));
+    m_list.insert(row, item);
+    endInsertRows();
 }
 
 void ListModel::handleItemChange()
 {
-  PlateItem* item = static_cast<PlateItem*>(sender());
-  QModelIndex index = indexFromItem(item);
-  if(index.isValid())
-    emit dataChanged(index, index);
+    PlateItem* item = static_cast<PlateItem*>(sender());
+    QModelIndex index = indexFromItem(item);
+
+    if (index.isValid())
+        emit dataChanged(index, index);
 }
 
 PlateItem * ListModel::find(const QString &id) const
 {
-  foreach(PlateItem* item, m_list) {
-    if(item->id() == id) return item;
-  }
-  return 0;
+    foreach (PlateItem* item, m_list)
+        if(item->id() == id) return item;
+
+    return 0;
 }
 
 int ListModel::checkIfNumbersNow(QString data)
 {
-    if(data.length() > 2)
-        if(data.at(2).isDigit())
-            return 3;
-    if(data.length() > 3)
-        if(data.at(3).isDigit())
-            return 4;
+    if (data.length() > 2 && data.at(2).isDigit())
+        return 3;
+    if (data.length() > 3 && data.at(3).isDigit())
+        return 4;
 
     return -1;
 }
@@ -161,47 +163,58 @@ void ListModel::searchFor(const QString &value, bool mode)
 
 QModelIndex ListModel::indexFromItem(const PlateItem *item) const
 {
-  Q_ASSERT(item);
-  for(int row=0; row<m_list.size(); ++row) {
-    if(m_list.at(row) == item) return index(row);
-  }
-  return QModelIndex();
+    Q_ASSERT(item);
+
+    for(int row=0; row<m_list.size(); ++row)
+        if(m_list.at(row) == item) return index(row);
+
+    return QModelIndex();
 }
 
 void ListModel::clear()
 {
-  qDeleteAll(m_list);
-  m_list.clear();
+    qDeleteAll(m_list);
+    m_list.clear();
 }
 
 bool ListModel::removeRow(int row, const QModelIndex &parent)
 {
-  Q_UNUSED(parent);
-  if(row < 0 || row >= m_list.size()) return false;
-  beginRemoveRows(QModelIndex(), row, row);
-  delete m_list.takeAt(row);
-  endRemoveRows();
-  return true;
+    Q_UNUSED(parent);
+
+    if (row < 0 || row >= m_list.size())
+        return false;
+
+    beginRemoveRows(QModelIndex(), row, row);
+    delete m_list.takeAt(row);
+    endRemoveRows();
+
+    return true;
 }
 
 bool ListModel::removeRows(int row, int count, const QModelIndex &parent)
 {
-  Q_UNUSED(parent);
-  if(row < 0 || (row+count) >= m_list.size()) return false;
-  beginRemoveRows(QModelIndex(), row, row+count-1);
-  for(int i=0; i<count; ++i) {
-    delete m_list.takeAt(row);
-  }
-  endRemoveRows();
-  return true;
+    Q_UNUSED(parent);
+
+    if (row < 0 || (row+count) >= m_list.size())
+        return false;
+
+    beginRemoveRows(QModelIndex(), row, row+count-1);
+
+    for (int i=0; i<count; ++i)
+        delete m_list.takeAt(row);
+
+    endRemoveRows();
+
+    return true;
 }
 
 PlateItem * ListModel::takeRow(int row)
 {
-  beginRemoveRows(QModelIndex(), row, row);
-  PlateItem* item = m_list.takeAt(row);
-  endRemoveRows();
-  return item;
+    beginRemoveRows(QModelIndex(), row, row);
+    PlateItem* item = m_list.takeAt(row);
+    endRemoveRows();
+
+    return item;
 }
 
 void ListModel::setSettings(Settings *settings)
